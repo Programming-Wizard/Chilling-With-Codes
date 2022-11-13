a2f112754aa7fd642d954de4e72da1da6eae612b
Varun Visave <117927436+Programming-Wizard@users.noreply.github.com>
2022-11-14 00:03:21 +0530

    finally can commit directly from eclipse

--- Practice Folder/src/hello_world.java
+++ Practice Folder/src/hello_world.java
@@ -1,6 +1,6 @@
 
 public class hello_world {
 	public static void main(String[] args) {
-		System.out.println("not that very hello world");
+		System.out.println("Finally i was able to commit directly from eclipse huehuehuehue");
 	}
 }
